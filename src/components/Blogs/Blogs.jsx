3fba157c58766b0e21eb6bbeec4060bdfec1c508
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-11-02 20:04:52 +0600

    loaded cover picture of title

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -10,6 +11,13 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs:{blogs.length} </h1>
+            {
+                blogs.map(blog => <Blog 
+                    key={blog.id} 
+                    blog={blog}>
+
+                </Blog>)
+            }
         </div>
     );
 };
